654510b59b9d361c85751079968bd2ae60c08fe2
Kielar <kielar.mm@proton.me>
2025-10-24 14:05:01 +0200

    Moved other pages to JSX and split header into separate component.

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -1,6 +1,8 @@
 import { Routes, Route } from 'react-router';
 import { HomePage } from './pages/HomePage';
 import { CheckoutPage } from './pages/CheckoutPage';
+import { OrdersPage } from './pages/OrdersPage';
+import { TrackingPage } from './pages/TrackingPage';
 import './App.css'
 
 function App() {
@@ -14,6 +16,14 @@ function App() {
       < Route 
         path="checkout" 
         element={<CheckoutPage />}
+      />
+      < Route 
+        path="orders" 
+        element={<OrdersPage />}
+      />
+      < Route 
+        path="tracking" 
+        element={<TrackingPage />}
       />
     </Routes>
   )
