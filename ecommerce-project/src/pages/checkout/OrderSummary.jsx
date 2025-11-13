98008e2ed20ab7090bb2f9135083d5ea54c8b743
Kielar <kielar.mm@proton.me>
2025-11-13 13:14:46 +0100

    Create Delete Cart Item feature

--- ecommerce-project/src/pages/checkout/OrderSummary.jsx
+++ ecommerce-project/src/pages/checkout/OrderSummary.jsx
@@ -19,6 +19,7 @@ export function OrderSummary({cart, deliveryOptions, loadCart}) {
 
                 <CartItemDetails 
                   cartItem={cartItem}
+                  loadCart={loadCart}
                 />
 
                 <DeliveryOptions 
